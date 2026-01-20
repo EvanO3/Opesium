08d19d62a478f2c79b81e36e8838419da0eee89d
Marcus Sisouphanh <107326899+msisouphanh@users.noreply.github.com>
2026-01-19 22:29:10 -0500

    Added place holder functions for user model

--- src/Models/User.ts
+++ src/Models/User.ts
@@ -4,8 +4,11 @@ export interface User {
   id: string;
   email: string;
   created_at: string;
+  updated_at: string;
 }
 
 export function createUser(email: string) {}
-export function updateEmail(id: string, newEmail: string) {}
+export function updateEmail(id: string, newEmail: string, updated_at: string) {}
 export function deleteUser(id: string) {}
+
+module.exports = { createUser, updateEmail, deleteUser };
