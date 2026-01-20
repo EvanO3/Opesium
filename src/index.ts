e2eec8e86574e17cbec5a7ae6d846e9f93853029
”EvanO3” <evan.oni@hotmail.com>
2026-01-19 19:29:41 -0500

    Inital commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, {Application, Request, Response} from "express"
+
+const app: Application = express();
+
+const PORT :number = 3000
+
+app.get("/", (req: Request, res: Response)=>{
+    res.json({message:"Typescript node is working"})
+});
+
+
+app.listen(PORT, ()=>{
+    console.log(`Listening to app on port ${PORT}`)
+});
