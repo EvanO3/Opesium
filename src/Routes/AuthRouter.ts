9bf560999fa2a489a45a8fbab5b7462a5ee0340f
”EvanO3” <evan.oni@hotmail.com>
2026-01-21 22:13:26 -0500

    added test auth route

--- src/Routes/AuthRouter.ts
+++ src/Routes/AuthRouter.ts
@@ -1,12 +1,17 @@
 import express from "express"
-import {UserCreation, LoginUser} from "../Controller/AuthController"
+import {UserCreation, LoginUser, SignOutUser, verifiedRoute} from "../Controller/AuthController"
+import { Request, Response} from "express"
+
 const router = express.Router();
 
 
 
 router.post("/signup", UserCreation)
 router.post("/login", LoginUser)
-
+router.post("/signout", SignOutUser)
+router.get("/auth", verifiedRoute, (req: Request, res: Response)=>{
+    res.status(200).json({message: "Welcome"})
+});
 
 
 export default {router}
