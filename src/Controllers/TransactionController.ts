9513783103cc45602472dffcf86071269d75b047
Marcus Sisouphanh <107326899+msisouphanh@users.noreply.github.com>
2026-01-23 11:19:13 -0500

    new changes

--- src/Controllers/TransactionController.ts
+++ src/Controllers/TransactionController.ts
@@ -4,8 +4,8 @@ import type { Request, Response } from "express";
 
 export const CreateTransaction = async (req: Request, res: Response) => {
   try {
-    const { user_id, categoryName, amount, description } = req.body;
-    await addTransaction(user_id, categoryName, amount, description);
+    const { categoryName, amount, description } = req.body;
+    await addTransaction(categoryName, amount, description);
     return res.status(200).json({ message: "successful transaction" });
   } catch (error) {
     return res.status(500).json({ message: "Internal server error: ", error });
