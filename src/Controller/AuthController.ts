630dd911a27775e74958ab8efc172bd1207fa70a
”EvanO3” <evan.oni@hotmail.com>
2026-01-24 00:35:18 -0500

    clean up activities

--- src/Controller/AuthController.ts
+++ src/Controller/AuthController.ts
@@ -1,7 +1,25 @@
 import { CreateUser, login, SignOut } from "../Service/AuthService";
 import verifyProjectJwt from "../Middleware/Jwt"
 import {NextFunction, Request, Response} from "express"
-import { STATUS_CODES } from "http";
+
+/**Helper function for catch block */
+
+function getErrorMessage(error : unknown): string{
+    if (error instanceof Error){
+        return error.message
+    }
+
+    if(typeof error === 'string'){
+        return error
+    }
+
+    /*fall back if none of the match's checkout*/
+
+    return "An unexpected Error occured"
+}   
+
+
+
 
 
 /**Refactor to throw meaningful error codes not only 201 and 500 */
@@ -10,16 +28,19 @@ const UserCreation = async (req: Request, res:Response) =>{
     try{
     const {name, email, password} = req.body;
     const userInfo = await CreateUser({name, email, password});
-    return res.status(201).json({message:userInfo})
+    return res.status(201).json({message:userInfo});
 
     }catch(error){
-    console.log("This is the error point ", error)
-     return  res.status(500).json({message:"Internal Server Error :" , error} )
+    console.log("This is the error point ", error);
+    const errorMessage = getErrorMessage(error);
+     return  res.status(400).json({error:"Error occured: " + errorMessage} )
         
     }
 } 
 
 
+/*TODO:
+    Maybe add more logic for catch block to remove double error message*/
 const LoginUser = async (req: Request, res: Response)=>{
     try{
         const {email, password}= req.body;
@@ -29,7 +50,8 @@ const LoginUser = async (req: Request, res: Response)=>{
 
     }catch(error){
         console.log("An unexpected Error has occured " + error)
-        return res.status(500).json({message:"An unexpected Error has occured: " , error})
+        const errorMessage = getErrorMessage(error);
+        return res.status(401).json({error:"Incorrect Email or Password:  " , errorMessage})
     }
 }
 
@@ -43,9 +65,13 @@ const SignOutUser = async (req: Request, res: Response)=>{
         return res.status(200).json({message:signOutMessage})
 
     }catch(error){
-        return res.status(400).json({message: "Failed to sign out user: " + error})
+        const errorMessage = getErrorMessage(error);
+        return res.status(400).json({message: "Failed to sign out user: " + errorMessage})
     }
 }
+
+
+
 
 
 const verifiedRoute = async (req:Request, res:Response, next:NextFunction)=>{
@@ -58,7 +84,9 @@ const verifiedRoute = async (req:Request, res:Response, next:NextFunction)=>{
          next();
 
     }catch(error){
-           console.log("Error Verifying Jwt: ", error)
+
+           const errorMessage = getErrorMessage(error);
+           console.log("Error Verifying Jwt: ", errorMessage)
            return res.status(401).json({message:"Unauthorized"});
 
     }
