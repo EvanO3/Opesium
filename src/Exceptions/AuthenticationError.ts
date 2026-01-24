d47caf2f00c33b6d7264083fe6d1da4e2bdbe645
”EvanO3” <evan.oni@hotmail.com>
2026-01-24 18:29:01 -0500

    custom exceptions

---
+++ src/Exceptions/AuthenticationError.ts
@@ -0,0 +1,9 @@
+export class AuthenticationError extends Error{
+    constructor(message:string){
+        super(message);
+        this.name = 'AuthenticationError';
+
+
+        Object.setPrototypeOf(this, AuthenticationError.prototype);
+    }
+}
