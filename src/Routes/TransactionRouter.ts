e0af6086e5e4ce1d3d282c2f593ead7268758c70
”EvanO3” <evan.oni@hotmail.com>
2026-01-25 00:17:23 -0500

    added update route

--- src/Routes/TransactionRouter.ts
+++ src/Routes/TransactionRouter.ts
@@ -1,11 +1,12 @@
 import express from 'express'
-import { CreateTransaction, RetrieveUsersTransactions } from '../Controller/TransactionController';
+import { CreateTransaction, RetrieveUsersTransactions, updateUserTransction } from '../Controller/TransactionController';
 import {verifiedRoute} from "../Controller/AuthController"
 const router = express.Router()
 
 
 router.post("/transactions/add",verifiedRoute, CreateTransaction);
 router.get("/transactions", verifiedRoute, RetrieveUsersTransactions )
+router.put("/transactions/:transactionId",verifiedRoute, updateUserTransction)
 
 export default {router}
 
