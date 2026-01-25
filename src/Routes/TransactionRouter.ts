7053713b77f5f2f162af3d9a5677a8ede6b46019
”EvanO3” <evan.oni@hotmail.com>
2026-01-24 22:56:36 -0500

    transaction route

--- src/Routes/TransactionRouter.ts
+++ src/Routes/TransactionRouter.ts
@@ -1,11 +1,11 @@
 import express from 'express'
-import { CreateTransaction } from '../Controller/TransactionController';
+import { CreateTransaction, RetrieveUsersTransactions } from '../Controller/TransactionController';
 import {verifiedRoute} from "../Controller/AuthController"
 const router = express.Router()
 
 
 router.post("/transactions/add",verifiedRoute, CreateTransaction);
-
+router.get("/transactions", verifiedRoute, RetrieveUsersTransactions )
 
 export default {router}
 
