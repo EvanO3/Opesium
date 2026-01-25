17d7fd6b2aaf7245af150d58ff8cfe6d9b193829
”EvanO3” <evan.oni@hotmail.com>
2026-01-25 01:21:20 -0500

    Delete route added

--- src/Routes/TransactionRouter.ts
+++ src/Routes/TransactionRouter.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { CreateTransaction, RetrieveUsersTransactions, updateUserTransction } from '../Controller/TransactionController';
+import { CreateTransaction, RetrieveUsersTransactions, updateUserTransction, deleteUsersTransaction } from '../Controller/TransactionController';
 import {verifiedRoute} from "../Controller/AuthController"
 const router = express.Router()
 
@@ -7,6 +7,7 @@ const router = express.Router()
 router.post("/transactions/add",verifiedRoute, CreateTransaction);
 router.get("/transactions", verifiedRoute, RetrieveUsersTransactions )
 router.put("/transactions/:transactionId",verifiedRoute, updateUserTransction)
+router.delete("/transactions/:transactionId",verifiedRoute, deleteUsersTransaction)
 
 export default {router}
 
